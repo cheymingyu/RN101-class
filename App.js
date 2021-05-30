ab893d3c2750eb6bd64856f9ecb8e2782216545e
dlrudfh <rudfh5256@naver.com>
2021-05-30 23:59:13 +0900

    06. state: counter

--- App.js
+++ App.js
@@ -1,17 +1,28 @@
 
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Button, StyleSheet, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-    const checkHandler = () => console.log('Clicked!');
+export default class App extends React.Component {
+    state = {
+        counter: 0,
+     };
 
-    return (
+     render() {
+       const clickHandler = () => {
+         this.setState({
+           counter: this.state.counter + 1,
+         });
+       };
+
+       return (
         <View style={styles.container}>
-            <Button title={'click me!'} onPress={() => console.log(clickHandler)} />
-            <StatusBar style="auto" />
+            <Text>{this.state.counter}</Text>
+            <Button title={'click me!'} onPress={clickHandler}/>
+          <StatusBar style="auto" />
         </View>
     );
+  }
 }
 
 const styles = StyleSheet.create({
