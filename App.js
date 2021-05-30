0a4fc8dac77d36474cc76b7407aa2f43ae1c4019
dlrudfh <rudfh5256@naver.com>
2021-05-30 23:52:35 +0900

    05. handler: function

--- App.js
+++ App.js
@@ -4,9 +4,12 @@ import React from 'react';
 import { Button, StyleSheet, View } from 'react-native';
 
 export default function App() {
+    const checkHandler = () => console.log('Clicked!');
+
     return (
         <View style={styles.container}>
-            <Button title={'click me!'} onPress={() => console.log('Clicked!')} />            <StatusBar style="auto" />
+            <Button title={'click me!'} onPress={() => console.log(clickHandler)} />
+            <StatusBar style="auto" />
         </View>
     );
 }
