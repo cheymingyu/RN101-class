6b77c19c584d052228140ba483e5f18569626242
parksubo <subo0521@hanmail.net>
2021-05-30 22:33:44 +0900

    02. Style: big text

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
+
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Button, StyleSheet, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello, world!</Text>
-      <Button title={'click me!'} onPress={() => console.log('Clicked!')}/>
-      <StatusBar style="auto" />
+      <Text style={styles.textBig}>Hello, world!</Text>
+      <Stat usBar style="auto" />
     </View>
   );
 }
@@ -18,5 +18,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    textBig: {
+      fontSize : 32
+    }
   },
 });
